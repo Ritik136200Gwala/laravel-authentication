251623efc5778a0ddd1b19ae16d37599bd560cf0
Ritik Gwala <ritik.gwala1362000@gmail.com>
2024-01-17 22:40:09 +0530

    Project uploaded on github

---
+++ app/Http/Controllers/GoogleAuthenticationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class GoogleAuthenticationController extends Controller
+{
+    public function setGoogleClient(Request $request){
+        
+    }
+}
